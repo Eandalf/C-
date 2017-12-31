15d785e14a358416464d389eaa692e4b1dab4e2b
Xandalf <32126310+Eandalf@users.noreply.github.com>
2017-12-31 19:37:00 +0800

    Add files via upload

---
+++ static array testing.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	static int a[4]={7,8,9},b[4]={4,5,6},c[4],i;
+	
+	for(i=0;i<3;i++)
+	{
+		c[i]=a[i]+b[i];
+		cout<<c[i]<<" ";
+	}
+	cout<<endl;
+	return 0;
+	
+	// execution result:
+	// 11 13 15
+}
