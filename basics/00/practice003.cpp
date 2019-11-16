803824d9334d95361e76be4070f6245e53052dd4
Eandalf <32126310+Eandalf@users.noreply.github.com>
2019-11-17 01:42:28 +0800

    Update practice003.cpp

--- basics/00/practice003.cpp
+++ basics/00/practice003.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
     int a=8,b=3,c=9;
     a=a+++(a-b)*(c-b);
     cout <<"a="<<a<<"\n"<<"b="<<b<<"\n"<<"c="<<c<<"\n";
     system("pause");
     return 0;
-    }
+}
