5837e215db5235aee2fea57eafe0d81c50a15db8
Eandalf <32126310+Eandalf@users.noreply.github.com>
2019-11-17 01:41:57 +0800

    Update practice001.cpp

--- basics/00/practice001.cpp
+++ basics/00/practice001.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
     float Y;
     cout<<"Please enter a number."<<"\n";
     cin>>Y;
     cout<<"The cube of the number is "<<Y*Y*Y<<"."<<"\n";
     system ("pause");
     return 0;
-    }
+}
