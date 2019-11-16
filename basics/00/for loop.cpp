560a0283749fb0235dcd31e9c837f2d2e27f95c0
Eandalf <32126310+Eandalf@users.noreply.github.com>
2019-11-17 01:39:42 +0800

    Update for loop.cpp

--- basics/00/for loop.cpp
+++ basics/00/for loop.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
     float sum=1;
-    for (int i=1;i<65;i++)
-    {
+    for (int i=1;i<65;i++){
         sum*=i;
         cout<<"i="<<i<<"\n"<<i<<"!="<<sum<<"\n";
-        }
-        system("pause");
-        return 0;
     }
+    system("pause");
+    return 0;
+}
