48933d4ae712ab23f91910f324d5e9aaef171e2b
Eandalf <32126310+Eandalf@users.noreply.github.com>
2019-11-17 01:45:13 +0800

    Update while loop.cpp

--- basics/00/while loop.cpp
+++ basics/00/while loop.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
     int sum=0,t,n=1;
     cout<<"Enter a number.\n";
     cin>>t;
-    while(n<=t)
-    {
-                 sum+=n;
-                 n+=1;
-                 }
-                 cout<<"1+2+3+......+"<<t<<"="<<sum<<endl;
-                 system("pause");
-                 return 0;
+    while(n<=t){
+        sum+=n;
+        n+=1;
     }
+    cout<<"1+2+3+......+"<<t<<"="<<sum<<endl;
+    system("pause");
+    return 0;
+}
