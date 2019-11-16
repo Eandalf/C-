518384ba9dbf95afd155bbf983f65e8c43dbba51
Eandalf <32126310+Eandalf@users.noreply.github.com>
2019-11-17 01:37:41 +0800

    Update V字形.cpp

--- basics/00/V字形.cpp
+++ basics/00/V字形.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     int K;
     cout<<"V\n";
     cout<<"Please enter a number.\n";
@@ -18,4 +17,4 @@ int main()
          }
     system ("pause");
     return 0;
-    }
+}
