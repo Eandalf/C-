06d08f7e6680f1a1a1a6b71553c68dd5a31c5e44
Eandalf <clinah@connect.ust.hk>
2020-04-24 18:15:38 -0400

    fix left encoding problems

--- basics/00/ASC2符號.cpp
+++ basics/00/ASC2符號.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
 	char c = 'a';
 	int i;
-	cout<<"輸入一個整數:";
+	cout<<"please enter an integer:";
 	cin>>i;
 	cout<<i<<endl;
 	c += i;
-	cout<<"此字元為:"<<c<<endl;
+	cout<<"the character is:"<<c<<endl;
 	//system("pause");
 	return 0;
 }
